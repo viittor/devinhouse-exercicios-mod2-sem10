01b9baa6514cdc384d6a1932c738f9d45c5bbde3
Victor Ferrando <victor.ferrando@hotmail.com>
2022-03-31 23:22:52 -0300

    exercises 1 to 4 solved

--- src/main/java/com/viittor/Semana10/repository/ProdutoRepository.java
+++ src/main/java/com/viittor/Semana10/repository/ProdutoRepository.java
@@ -1,6 +1,8 @@
 package com.viittor.Semana10.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends CrudRepository<ProdutoRepository, Long> {
 }
--- src/main/java/com/viittor/Semana10/repository/UsuarioRepository.java
+++ src/main/java/com/viittor/Semana10/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.viittor.Semana10.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends CrudRepository<UsuarioRepository, Long> {
+
 }
